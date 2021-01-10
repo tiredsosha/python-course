ed21275b80a821df67bb1af1e8059ea7ed8fe478
Sosha <sasha.che007@gmail.com>
2021-01-10 16:20:44 +0300

    ticket

---
+++ ticket.py
@@ -0,0 +1,7 @@
+num = input()
+summf = (int(num[0]) + int(num[1]) + int(num[2]))
+summl = (int(num[3]) + int(num[4]) + int(num[5]))
+if summf == summl:
+    print('Счастливый')
+else:
+    print('Обычный')
